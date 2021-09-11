8ab18211bb876b4d4f91d6f57e80346b84ee4efe
DarkLord <dfhrhfan12@yandex.ru>
2021-09-11 23:49:49 +0400

    Add old version

--- new/includes/head_includes.js
+++ new/includes/head_includes.js
@@ -1,6 +1,5 @@
 import { addMetaData, addElement } from "./dom_functions.js";
 
-
 let headElement = document.getElementsByTagName('head')[0];
 addMetaData(headElement, { charset: 'utf-8' });
 addMetaData(headElement, { name: 'description', content: 'Общественная организация по занятиям Айкидо Айкикай в Саратове' });
@@ -17,3 +16,49 @@ addMetaData(headElement, {
 
 addElement(headElement, 'title', undefined, 'Саратовская Федерация Айкидо Айкикай');
 addElement(headElement, 'link', { rel: 'stylesheet', href: '/styles/MyStyle.css' });
+
+/*
+<meta name="description" content="Общественная организация по занятиям Айкидо Айкикай в Саратове">
+<meta name="keywords" content="айкидо, саратов, айкидо саратов, клубы айкидо, психологическое айкидо,
+                                        школы айкидо, школа айкидо, секция айкидо, занятия айкидо, семинары айкидо,
+                                        айкидо центры, айкидо России, секция айкидо саратов, клуб айкидо саратов,
+                                        что такое айкидо, Станислав Петров, Михаил Сафронов, Кристиан Тисье,
+                                        Кристиан Тиссье, Патрик Бенези, Сейширо Эндо, бокен, танто, атеми, укеми
+                                        aikido, aikikai, Stanislav Petrov, Mikhail Safronov, Christian Tissier,
+                                        Patrick Behezi, aikiken, айкидо для детей, спорт, рукопашный бой, самооборона">
+
+
+
+<meta charset="utf-8">
+<title>Саратовская Федерация Айкидо Айкикай</title>
+<link rel="stylesheet" href="/styles/MyStyle.css">
+
+<script>
+    $(
+        function () {
+            $('#head').load('/includes/head.inc')
+        }
+    );
+</script>
+<script>
+    $(
+        function () {
+            $('#menu').load('/includes/menu.inc')
+        }
+    );
+</script>
+<script>
+    $(
+        function () {
+            $('#right').load('/includes/right.inc')
+        }
+    );
+</script>
+<script>
+    $(
+        function () {
+            $('#creators').load('/includes/creators.inc')
+        }
+    );
+</script>
+*/
