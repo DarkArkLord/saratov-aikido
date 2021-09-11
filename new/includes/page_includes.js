745800915c488340c5646ae5bb4896beebf416e8
DarkLord <dfhrhfan12@yandex.ru>
2021-09-12 01:29:49 +0400

    Add new page template

--- new/includes/page_includes.js
+++ new/includes/page_includes.js
@@ -1,3 +1,65 @@
-import './head_includes.js';
-import './header_includes.js';
-import './menu_includes.js';
+// import './head_includes.js';
+// import './header_includes.js';
+// import './menu_includes.js';
+
+function createColumnForParent(parent) {
+    let columnElement = document.createElement('td');
+    parent.appendChild(columnElement);
+    return columnElement;
+}
+
+function createRowForParent(parent) {
+    let rowElement = document.createElement('tr');
+    parent.appendChild(rowElement);
+    return {
+        element: rowElement,
+        addColumn: () => createColumnForParent(rowElement)
+    };
+}
+
+function createTableForParent(parent) {
+    let tableElement = document.createElement('table');
+    parent.appendChild(tableElement);
+    return {
+        element: tableElement,
+        addRow: () => createRowForParent(tableElement)
+    };
+}
+
+function getTableById(id) {
+    let tableElement = document.getElementById(id);
+    return {
+        element: tableElement,
+        addRow: () => createRowForParent(tableElement)
+    };
+}
+
+const mainPageTable = getTableById('mainPageTable');
+const mainPageContent = document.getElementById('mainPageContent');
+
+mainPageTable.element.innerHTML = '';
+
+(function() {
+    const header = mainPageTable.addRow().addColumn();
+    header.setAttribute('colspan', 3);
+    header.innerHTML = 'header';
+})();
+
+(function() {
+    const body = mainPageTable.addRow();
+
+    const menu = body.addColumn();
+    menu.innerHTML = 'menu';
+
+    const content = body.addColumn();
+    content.append(mainPageContent);
+
+    const right = body.addColumn();
+    right.innerHTML = 'right';
+})();
+
+(function() {
+    const footer = mainPageTable.addRow().addColumn();
+    footer.setAttribute('colspan', 3);
+    footer.innerHTML = 'footer';
+})();
