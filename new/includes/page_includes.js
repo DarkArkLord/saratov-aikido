a932a33f1e1f59b92b219824966acbdc92c537e5
DarkLord <dfhrhfan12@yandex.ru>
2021-09-13 00:01:47 +0400

    Upd page includes

--- new/includes/page_includes.js
+++ new/includes/page_includes.js
@@ -1,70 +1,13 @@
 import './head_includes.js';
 
 import { HTMLTags, ItemTypes, render } from "./render_lib.js";
+import { headerItem } from './header_includes.js';
+import { footerItem } from './footer_includes.js';
 
-function createColumnForParent(parent) {
-    let columnElement = document.createElement("td");
-    parent.appendChild(columnElement);
-    return columnElement;
-}
-
-function createRowForParent(parent) {
-    let rowElement = document.createElement("tr");
-    parent.appendChild(rowElement);
-    return {
-        element: rowElement,
-        addColumn: () => createColumnForParent(rowElement),
-    };
-}
-
-function createTableForParent(parent) {
-    let tableElement = document.createElement("table");
-    parent.appendChild(tableElement);
-    return {
-        element: tableElement,
-        addRow: () => createRowForParent(tableElement),
-    };
-}
-
-function getTableById(id) {
-    let tableElement = document.getElementById(id);
-    return {
-        element: tableElement,
-        addRow: () => createRowForParent(tableElement),
-    };
-}
-
-// const mainPageTable = getTableById('mainPageTable');
 const mainPageContent = document.getElementById('mainPageContent');
+const rightPageContent = document.getElementById('rightPageContent');
 
-// mainPageTable.element.innerHTML = '';
-
-// (function() {
-//     const header = mainPageTable.addRow().addColumn();
-//     header.setAttribute('colspan', 3);
-//     header.innerHTML = 'header';
-// })();
-
-// (function() {
-//     const body = mainPageTable.addRow();
-
-//     const menu = body.addColumn();
-//     menu.innerHTML = 'menu';
-
-//     const content = body.addColumn();
-//     content.append(mainPageContent);
-
-//     const right = body.addColumn();
-//     right.innerHTML = 'right';
-// })();
-
-// (function() {
-//     const footer = mainPageTable.addRow().addColumn();
-//     footer.setAttribute('colspan', 3);
-//     footer.innerHTML = 'footer';
-// })();
-
-let item = {
+let pageItem = {
     tag: HTMLTags.Table,
     attributes: { border: "1", cellpadding: "0", cellspacing: "0", width: "100%" },
     type: ItemTypes.Container,
@@ -75,9 +18,9 @@ let item = {
             childs: [
                 {
                     tag: HTMLTags.TableData,
-                    type: ItemTypes.Value,
-                    attributes: { colspan: 3 },
-                    value: 'header',
+                    type: ItemTypes.Container,
+                    attributes: { width: "100%" },
+                    childs: [headerItem]
                 }
             ],
         },
@@ -87,22 +30,37 @@ let item = {
             childs: [
                 {
                     tag: HTMLTags.TableData,
-                    type: ItemTypes.Value,
-                    value: 'menu',
-                },
-                {
-                    tag: HTMLTags.TableData,
                     type: ItemTypes.Container,
                     childs: [
                         {
-                            element: mainPageContent,
+                            tag: HTMLTags.Table,
+                            type: ItemTypes.Container,
+                            attributes: { border: "1", cellpadding: "0", cellspacing: "0", width: "100%" },
+                            childs: [
+                                {
+                                    tag: HTMLTags.TableRow,
+                                    type: ItemTypes.Container,
+                                    childs: [
+                                        {
+                                            tag: HTMLTags.TableData,
+                                            type: ItemTypes.Value,
+                                            value: 'menu',
+                                        },
+                                        {
+                                            tag: HTMLTags.TableData,
+                                            type: ItemTypes.HtmlElementContainer,
+                                            innerElement: mainPageContent
+                                        },
+                                        {
+                                            tag: HTMLTags.TableData,
+                                            type: ItemTypes.HtmlElementContainer,
+                                            innerElement: rightPageContent
+                                        }
+                                    ]
+                                }
+                            ]
                         }
                     ]
-                },
-                {
-                    tag: HTMLTags.TableData,
-                    type: ItemTypes.Value,
-                    value: 'smth',
                 }
             ]
         },
@@ -112,15 +70,15 @@ let item = {
             childs: [
                 {
                     tag: HTMLTags.TableData,
-                    type: ItemTypes.Value,
-                    attributes: { colspan: 3 },
-                    value: 'footer',
+                    type: ItemTypes.Container,
+                    attributes: { height: "20", colspan: "2", align: "right", valign: "bottom" },
+                    childs: [footerItem]
                 }
             ],
         },
     ],
 };
 
-document.body.innerHTML = "";
-let t = render(item);
-document.body.append(t);
+document.body.innerHTML = '';
+let pageElement = render(pageItem);
+document.body.append(pageElement);
