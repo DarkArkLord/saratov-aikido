2187fc0a6b33d88eaefa78e463de583f037953f2
DarkLord <dfhrhfan12@yandex.ru>
2022-02-19 23:53:38 +0400

    Upd menu items

--- new/includes/menu_elements.js
+++ new/includes/menu_elements.js
@@ -1,19 +1,19 @@
-export const newsElements = [
-    { title: 'Приглашаем на занятия по айкидо в "Эверест"!', link: 'https://vk.com/wall-3928256_2143' },
-    { title: 'С Новым 2022 годом!', link: 'https://vk.com/wall-3928256_2196' },
-    { title: 'Экзамен. Декабрь 2021.', link: 'https://vk.com/wall-3928256_2194' },
-    { title: 'Интервью Паскаля Гиймана: отход от дисциплины айкидо.', link: 'http://www.saratov-aikido.ru/pages/DMenu/DAikidoVSaratove/DArchive/DArticles/DInterviewPascal/PInterviewPascal.html' },
-    { title: 'Интерактивное обновление новостей', link: 'http://vk.com/aikidovsaratove' },
-];
-
 export const menuElements = [
     { title: 'Главная', link: '/index.html' },
     { title: 'Айкидо в Саратове', link: '/pages/DMenu/DAikidoVSaratove/PAikidoVSaratove.html' },
     { title: 'Наш проект "Aikido for all"', link: '/pages/DMenu/DSpecialProject/PSpecialProject.html' },
     { title: 'Детские группы', link: '/pages/DMenu/DChildren/PChildren.html' },
     { title: 'Залы и расписания', link: '/pages/DMenu/DZaliIRasp/PZaliIRasp.html' },
-    { title: 'Семинары', link: '/pages/DMenu/DSeminary/PSeminary.html' },
+    // { title: 'Семинары', link: '/pages/DMenu/DSeminary/PSeminary.html' },
     { title: 'Аттестации', link: '/pages/DMenu/DAttestation/PAttestation.html' },
     { title: 'Ссылки', link: '/pages/DMenu/DLinks/PLinks.html' },
     { title: 'Контакты', link: '/pages/DMenu/DContacts/PContacts.html' },
+];
+
+export const newsElements = [
+    { title: 'Изменение расписания работы залов федерации в 2022!', link: 'http://www.saratov-aikido.ru/pages/DMenu/DZaliIRasp/PZaliIRasp.html' },
+    { title: 'С Новым 2022 годом!', link: 'https://vk.com/wall-3928256_2196' },
+    { title: 'Экзамен. Декабрь 2021.', link: 'https://vk.com/wall-3928256_2194' },
+    { title: 'Интервью Паскаля Гиймана: отход от дисциплины айкидо.', link: 'http://www.saratov-aikido.ru/pages/DMenu/DAikidoVSaratove/DArchive/DArticles/DInterviewPascal/PInterviewPascal.html' },
+    { title: 'Интерактивное обновление новостей', link: 'http://vk.com/aikidovsaratove' },
 ];
