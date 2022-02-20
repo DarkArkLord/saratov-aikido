01ca03d60ebe827f0a16c5845d4a0bba57a6b13a
DarkLord <dfhrhfan12@yandex.ru>
2022-02-20 22:01:15 +0400

    Upd childrens

--- new/includes/menu_elements.js
+++ new/includes/menu_elements.js
@@ -2,7 +2,7 @@ export const menuElements = [
     { title: 'Главная +', link: '/index.html' },
     { title: 'Айкидо в Саратове +-', link: '/pages/DMenu/DAikidoVSaratove/PAikidoVSaratove.html' },
     { title: 'Наш проект "Aikido for all"', link: '/pages/DMenu/DSpecialProject/PSpecialProject.html' },
-    { title: 'Детские группы', link: '/pages/DMenu/DChildren/PChildren.html' },
+    { title: 'Детские группы +', link: '/pages/DMenu/DChildren/PChildren.html' },
     { title: 'Залы и расписания', link: '/pages/DMenu/DZaliIRasp/PZaliIRasp.html' },
     // { title: 'Семинары +', link: '/pages/DMenu/DSeminary/PSeminary.html' },
     { title: 'Аттестации +', link: '/pages/DMenu/DAttestation/PAttestation.html' },
