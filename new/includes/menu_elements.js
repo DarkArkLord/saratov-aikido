589df54e687a6f77ba066d1fe334130c647c7de3
DarkLord <dfhrhfan12@yandex.ru>
2022-07-12 00:03:14 +0400

    Fix zali&rasp

--- new/includes/menu_elements.js
+++ new/includes/menu_elements.js
@@ -1,13 +1,13 @@
 export const menuElements = [
-    { title: 'Главная +', link: '/index.html' },
-    { title: 'Айкидо в Саратове +-', link: '/pages/DMenu/DAikidoVSaratove/PAikidoVSaratove.html' },
+    { title: 'Главная', link: '/index.html' },
+    { title: 'Айкидо в Саратове', link: '/pages/DMenu/DAikidoVSaratove/PAikidoVSaratove.html' },
     { title: 'Наш проект "Aikido for all"', link: '/pages/DMenu/DSpecialProject/PSpecialProject.html' },
-    { title: 'Детские группы +', link: '/pages/DMenu/DChildren/PChildren.html' },
+    { title: 'Детские группы', link: '/pages/DMenu/DChildren/PChildren.html' },
     { title: 'Залы и расписания', link: '/pages/DMenu/DZaliIRasp/PZaliIRasp.html' },
-    // { title: 'Семинары +', link: '/pages/DMenu/DSeminary/PSeminary.html' },
-    { title: 'Аттестации +', link: '/pages/DMenu/DAttestation/PAttestation.html' },
-    { title: 'Ссылки +', link: '/pages/DMenu/DLinks/PLinks.html' },
-    { title: 'Контакты +', link: '/pages/DMenu/DContacts/PContacts.html' },
+    // { title: 'Семинары', link: '/pages/DMenu/DSeminary/PSeminary.html' },
+    { title: 'Аттестации', link: '/pages/DMenu/DAttestation/PAttestation.html' },
+    { title: 'Ссылки', link: '/pages/DMenu/DLinks/PLinks.html' },
+    { title: 'Контакты', link: '/pages/DMenu/DContacts/PContacts.html' },
 ];
 
 export const newsElements = [
